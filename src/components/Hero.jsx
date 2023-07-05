290e95647c0cdea45c910a8ba876e7ad96004ea6
WafaBenyettou <benwafa951@gmail.com>
2023-07-05 19:11:00 +0100

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => (
       />
 
       <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-      <div className="absolute z-[1] w-[20%] h-[80%] rounded-full white__gradient bottom-40" />
+      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
     </div>
     <div className={`ss:hidden ${styles.flexCenter}`}>
